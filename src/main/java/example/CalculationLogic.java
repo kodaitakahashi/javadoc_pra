3ec3e41a2efc20b28a7041612b1b0c26f877e9c2
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2018-06-22 13:49:30 +0900

    fix

--- src/main/java/example/CalculationLogic.java
+++ src/main/java/example/CalculationLogic.java
@@ -17,9 +17,9 @@ public final class CalculationLogic {
      * 二つの整数の足し算の結果を返します。
      * 
      * @param num1
-     *            足し算する整数
+     *            足し算する整数値
      * @param num2
-     *            足し算する整数
+     *            足し算する整数値
      * @return 足し算の結果を整数値で返します。
      */
     public static int add(int num1, int num2) {
@@ -27,7 +27,7 @@ public final class CalculationLogic {
     }
 
     /**
-     * 受け取った複数の整数の足し算の結果を返します。
+     * 受け取った複数の整数値の足し算の結果を返します。
      * 
      * 
      * <p>
@@ -51,16 +51,21 @@ public final class CalculationLogic {
      * </p>
      * 
      * <p>
-     * 基本型のラッパークラスの値を整数として足し算したい時に使用します。
+     * 基本型のラッパークラスの値を整数値として足し算したい時に使用します。
      * </p>
      * <p>
-     * int型で返すため、int型が扱える値を超えるとNumberFormatExceptionが発生します。
+     * <code>int</code>型で返すため、<code>int</code>型が扱える値より
+     * 大きな値を渡すと予期しない整数値になるため渡す値は気を付けてください。
      * </p>
      * 
      * @param num1
      *            足し算する<code>Number</code>オブジェクト <code>null</code>は許可しません
+     *            <code>null</code>の、<code>NullPointerException</code>が発生します。
+     * 
      * @param num2
      *            足し算する<code>Number</code>オブジェクト <code>null</code>は許可しません
+     *            <code>null</code>の場合、<code>NullPointerException</code>が発生します。
+     * 
      * @return 足し算の結果を整数値で返します。
      */
     public static int add(Number num1, Number num2) {
@@ -80,19 +85,22 @@ public final class CalculationLogic {
      * </p>
      * 
      * <p>
-     * <code>int</code>型で返すため、<code>int</code>型が扱える値を
-     * 超える値を渡すと予期しない整数値になるため渡す値は気を付けてください。
+     * <code>int</code>型で返すため、<code>int</code>型が扱える値より
+     * 大きな値を渡すと予期しない整数値になるため渡す値は気を付けてください。
      * </p>
      * 
      * @param numbers
      *            足し算をする<code>Number</code>オブジェクトの配列 <code>null</code>は許可しません。
      *            <p>
-     *            <code>null</code>が含まれた配列の場合、NullPointerExceptionが発生します。
+     *            <code>null</code>が含まれた配列の場合、<code>NullPointerException</code>が発生します。
      *            </p>
      * @return 足し算の結果を整数値で返します。
      */
     public static int add(Number[] numbers) {
         return Arrays.stream(numbers).mapToInt(Number::intValue).sum();
     }
+
+    // TODO 割り算の処理。
+    // ドキュメンテーションコメントを読めば使い方がわかるように意識すること
 
 }
